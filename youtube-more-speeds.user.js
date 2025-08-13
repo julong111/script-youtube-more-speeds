fe4263784615e9ad066dc6079cb59b0a177f8e0e
julong <julong@111.com>
2025-08-13 08:54:08 +0800

    [Update] 1.0.1
    新增 live 视频恢复速度

--- youtube-more-speeds.user.js
+++ youtube-more-speeds.user.js
@@ -1,44 +1,56 @@
 // ==UserScript==
-// @version      1.0.0
 // @name         YouTube More Speeds Modfied
-// @description  Adds buttons under a YouTube video with more playback speeds.  forked from: https://github.com/ssssssander/youtube-more-speeds  1.5.1
-
 // @name:zh YouTube上的更多速度
-// @description:zh 添加下一个YouTube视频与更多的播放速度按钮。  forked from: https://github.com/ssssssander/youtube-more-speeds  1.5.1
-
+// @version      1.0.1
 // @namespace    https://github.com/julong111/script-youtube-more-speeds
-// @icon https://www.youtube.com/s/desktop/3748dff5/img/favicon_48.png
 // @author       julong@111.com
 // @homepage https://github.com/julong111/script-youtube-more-speeds
 // @supportURL https://github.com/julong111/script-youtube-more-speeds/issues
 // @match        *://*.youtube.com/*
+// @icon https://www.youtube.com/s/desktop/3748dff5/img/favicon_48.png
+// @description  Adds buttons under a YouTube video with more playback speeds.  project forked from: https://github.com/ssssssander/youtube-more-speeds  1.5.1
+// @description:zh 添加下一个YouTube视频与更多的播放速度按钮。  forked from: https://github.com/ssssssander/youtube-more-speeds  1.5.1
+// @charset		 UTF-8
 // @license MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // BEGIN waitForKeyElements
-    /**
- * A utility function for userscripts that detects and handles AJAXed content.
- *
- * Usage example:
- *
- *     function callback(domElement) {
- *         domElement.innerHTML = "This text inserted by waitForKeyElements().";
- *     }
- *
- *     waitForKeyElements("div.comments", callback);
- *     // or
- *     waitForKeyElements(selectorFunction, callback);
- *
- * @param {(string|function)} selectorOrFunction - The selector string or function.
- * @param {function} callback - The callback function; takes a single DOM element as parameter.
- *                              If returns true, element will be processed again on subsequent iterations.
- * @param {boolean} [waitOnce=true] - Whether to stop after the first elements are found.
- * @param {number} [interval=300] - The time (ms) to wait between iterations.
- * @param {number} [maxIntervals=-1] - The max number of intervals to run (negative number for unlimited).
- */
+    function addSpeeds() {
+        if (funcDone) return;
+
+        let bgColor = colors[0];
+        let moreSpeedsDiv = document.createElement('div');
+        moreSpeedsDiv.id = 'more-speeds';
+
+        let speeds = [0.5, 0.75, 1.0, 1.5, 2.0, 3.0];
+
+        for (let i = 0; i < speeds.length; i++) {
+
+            if (speeds[i] >= 1) { bgColor = colors[1]; }
+            if (speeds[i] >= 3) { bgColor = colors[2]; }
+
+            let btn = document.createElement('button');
+            btn.style.backgroundColor = bgColor;
+            btn.style.marginRight = '4px';
+            btn.style.border = '1px solid #D3D3D3';
+            btn.style.borderRadius = '2px';
+            btn.style.color = '#ffffff';
+            btn.style.cursor = 'pointer';
+            btn.style.fontFamily = 'monospace';
+            btn.textContent = speeds[i].toString()+'×';
+
+            btn.addEventListener('click', () => { document.getElementsByTagName('video')[0].playbackRate = speeds[i] } );
+            moreSpeedsDiv.appendChild(btn);
+        }
+
+        let titleElem = document.querySelector(titleElemSelector);
+        titleElem.before(moreSpeedsDiv);
+
+        funcDone = true;
+    }
+
     function waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals) {
         if (typeof waitOnce === "undefined") {
             waitOnce = true;
@@ -77,51 +89,27 @@
             }, interval);
         }
     }
-    // END waitForKeyElements
 
-    // Actual code starts here
+    function checkClassPresence(selector, className) {
+      setInterval(() => {
+        const element = document.querySelector(selector);
+        if (element ) {
+            if( element.classList.contains(className)){
+                document.getElementsByTagName('video')[0].playbackRate = 1.0;
+            }
+        }
+      }, 1000);
+    }
+
+    checkClassPresence('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate.ytp-live > button', 'ytp-live-badge-is-livehead');
+    
     let funcDone = false;
-    // const titleElemSelector = 'div#title.style-scope.ytd-watch-metadata';
     const titleElemSelector = '#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-right-controls';
 
-    const colors = ['#072525', '#287F54', '#C22544']; // https://www.schemecolor.com/wedding-in-india.php
+    const colors = ['#072525', '#287F54', '#C22544'];
     if (!funcDone) window.addEventListener('yt-navigate-start', addSpeeds);
 
     if (document.body && !funcDone) {
         waitForKeyElements(titleElemSelector, addSpeeds);
-    }
-
-    function addSpeeds() {
-        if (funcDone) return;
-
-        let bgColor = colors[0];
-        let moreSpeedsDiv = document.createElement('div');
-        moreSpeedsDiv.id = 'more-speeds';
-
-        let speeds = [0.5, 0.75, 1.0, 1.5, 2.0, 3.0];
-
-        for (let i = 0; i < speeds.length; i++) {
-
-            if (speeds[i] >= 1) { bgColor = colors[1]; }
-            if (speeds[i] >= 3) { bgColor = colors[2]; }
-
-            let btn = document.createElement('button');
-            btn.style.backgroundColor = bgColor;
-            btn.style.marginRight = '4px';
-            btn.style.border = '1px solid #D3D3D3';
-            btn.style.borderRadius = '2px';
-            btn.style.color = '#ffffff';
-            btn.style.cursor = 'pointer';
-            btn.style.fontFamily = 'monospace';
-            btn.textContent = speeds[i].toString()+'×';
-
-            btn.addEventListener('click', () => { document.getElementsByTagName('video')[0].playbackRate = speeds[i] } );
-            moreSpeedsDiv.appendChild(btn);
-        }
-
-        let titleElem = document.querySelector(titleElemSelector);
-        titleElem.before(moreSpeedsDiv);
-
-        funcDone = true;
     }
 })();
